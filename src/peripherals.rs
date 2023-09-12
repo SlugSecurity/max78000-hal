1a22dc41ceaf58f12e759bc5a65738aebe0e0287
Brian Mak <eggrollbot@gmail.com>
2023-09-11 17:42:57 -0700

    Organize peripheral mod declaration by embedded HAL

--- src/peripherals.rs
+++ src/peripherals.rs
@@ -1,22 +1,25 @@
 //! Peripheral drivers for the MAX78000.
 
+// Embedded HAL peripherals.
 pub mod adc;
+pub mod delay;
+pub mod digital;
+pub mod rng;
+pub mod serial;
+pub mod timer;
+pub mod watchdog;
+
+// Non embedded HAL peripherals.
 pub mod aes;
 pub mod bit_banding;
 pub mod bootloader;
 pub mod crc;
-pub mod delay;
-pub mod digital;
 pub mod ecc;
 pub mod flash_controller;
 pub mod oscillator;
 pub mod power;
 pub mod raw;
-pub mod rng;
 pub mod rtc;
-pub mod serial;
 pub mod synchronization;
-pub mod timer;
-pub mod watchdog;
 
 // TODO: Create peripheral manager and traits.
