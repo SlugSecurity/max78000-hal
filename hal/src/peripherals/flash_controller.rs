e5ea34211e7d44fd17428f105e3fce0549c3c3ab
Brian Mak <eggrollbot@gmail.com>
2023-11-24 14:10:51 -0800

    Add flash controller skeleton

--- hal/src/peripherals/flash_controller.rs
+++ hal/src/peripherals/flash_controller.rs
@@ -1,1 +1,20 @@
 //! Flash controller peripheral API.
+
+use max78000::FLC;
+
+/// TRNG peripheral.
+pub struct FlashController {
+    flc: FLC,
+}
+
+// TODO: Implement with the peripheral API when available.
+
+impl FlashController {
+    /// Creates a new flash controller peripheral.
+    // TODO: Make this function pub(crate) when the peripheral API is available. Tests needs it public until then.
+    pub fn new(flc: FLC) -> Self {
+        Self { flc }
+    }
+
+    // TODO: Add functions for the flash controller.
+}
