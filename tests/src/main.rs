646570919becb3a9b29e333e911c5bdef8ebb748
Brian Mak <eggrollbot@gmail.com>
2023-11-24 02:31:10 -0800

    Finish TRNG tests

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -4,7 +4,10 @@
 #![no_main]
 #![no_std]
 
+use core::fmt::{self, Write};
+
 use cortex_m_rt::entry;
+use cortex_m_semihosting::hio;
 use max78000_hal::max78000::Peripherals;
 use tests::trng_tests;
 
@@ -15,9 +18,12 @@ pub mod tests;
 /// Entry point for tests.
 #[entry]
 fn main() -> ! {
+    let mut stdout = hio::hstdout().map_err(|_| fmt::Error).unwrap();
+    writeln!(stdout, "Starting MAX78000 HAL tests...\n").unwrap();
+
     // TODO: Use peripheral API when available.
     let peripherals = Peripherals::take().unwrap();
-    trng_tests::run_trng_tests(peripherals.TRNG);
+    trng_tests::run_trng_tests(peripherals.TRNG, &mut stdout);
 
     #[allow(clippy::empty_loop)]
     loop {}
--- tests/src/tests/trng_tests.rs
+++ tests/src/tests/trng_tests.rs
@@ -1,8 +1,42 @@
 //! Tests for the TRNG peripheral.
 
-use max78000_hal::{max78000::TRNG, peripherals::trng};
+use core::fmt::Write;
+
+use cortex_m_semihosting::hio;
+use max78000_hal::{max78000::TRNG, peripherals::trng::Trng};
 
 /// Runs all TRNG tests.
-pub fn run_trng_tests(trng: TRNG) {
-    trng::Trng::new(trng);
+pub fn run_trng_tests(trng_regs: TRNG, stdout: &mut hio::HostStream) {
+    writeln!(stdout, "Starting TRNG peripheral tests...").unwrap();
+
+    let trng = Trng::new(trng_regs);
+    test_random_u32(&trng);
+    test_fill_buffer(&trng, stdout);
+    writeln!(stdout, "TRNG peripheral tests complete!\n").unwrap();
+}
+
+/// Tests the [`trng::Trng::random_u32()`] function.
+fn test_random_u32(trng: &Trng) {
+    for _ in 0..100 {
+        let random = trng.random_u32();
+        assert_ne!(random, 0);
+    }
+}
+
+/// Tests the [`trng::Trng::fill_buffer()`] function.
+fn test_fill_buffer(trng: &Trng, stdout: &mut hio::HostStream) {
+    let mut buf = [0u8; 4];
+    trng.fill_buffer(&mut buf);
+    assert_ne!(buf, [0u8; 4]);
+    writeln!(stdout, "TRNG buffer: {:?}", buf).unwrap();
+
+    for _ in 0..100 {
+        let mut buf = [0u8; 103]; // Not a multiple of 32 bits.
+        trng.fill_buffer(&mut buf);
+        assert_ne!(buf, [0u8; 103]);
+
+        let mut buf = [0u8; 124]; // Multiple of 32 bits.
+        trng.fill_buffer(&mut buf);
+        assert_ne!(buf, [0u8; 124]);
+    }
 }
