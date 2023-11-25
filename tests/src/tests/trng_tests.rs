09c5fd42c4b44764e64a0239321527441b07df19
Brian Mak <eggrollbot@gmail.com>
2023-11-24 16:37:33 -0800

    Finish entropy tests

--- tests/src/tests/trng_tests.rs
+++ tests/src/tests/trng_tests.rs
@@ -24,7 +24,15 @@ pub fn run_trng_tests(trng_regs: TRNG, gcr_regs: &GCR, stdout: &mut hio::HostStr
     writeln!(stdout, "TRNG peripheral tests complete!\n").unwrap();
 }
 
-/// Calculates the minimum entropy of a buffer in bits per byte.
+/// Tests the [`trng::Trng::random_u32()`] function.
+fn test_random_u32(trng: &Trng) {
+    for _ in 0..100 {
+        let random = trng.random_u32();
+        assert_ne!(random, 0);
+    }
+}
+
+/// Calculates the min-entropy of a buffer in bits per byte.
 fn min_entropy(buf: &[u8]) -> f64 {
     let mut ones = 0;
 
@@ -42,35 +50,61 @@ fn min_entropy(buf: &[u8]) -> f64 {
     -libm::log2(one_probability.max(zero_probability)) * 8.0
 }
 
-/// Tests the [`trng::Trng::random_u32()`] function.
-fn test_random_u32(trng: &Trng) {
-    for _ in 0..100 {
-        let random = trng.random_u32();
-        assert_ne!(random, 0);
+/// Prints a warning if the min-entropy of a buffer is less than a threshold.
+fn find_and_warn_entropy(
+    stdout: &mut hio::HostStream,
+    buf: &[u8],
+    entropy_req: f64,
+    print_entropy: bool,
+) {
+    let entropy_actual = min_entropy(buf);
+
+    if print_entropy {
+        writeln!(
+            stdout,
+            "{}B TRNG buffer entropy: {} bits per byte",
+            buf.len(),
+            entropy_actual
+        )
+        .unwrap();
+    }
+
+    if entropy_actual < entropy_req {
+        writeln!(
+            stdout,
+            "Error: entropy bits per byte <{} on {}B buffer: {} bits per byte",
+            entropy_req,
+            buf.len(),
+            entropy_actual
+        )
+        .unwrap();
     }
 }
 
 /// Tests the [`trng::Trng::fill_buffer()`] function.
 fn test_fill_buffer(trng: &Trng, stdout: &mut hio::HostStream) {
+    // Small buffer test.
     let mut buf = [0u8; 15];
     trng.fill_buffer(&mut buf);
-    let mut entropy = min_entropy(&buf);
     writeln!(stdout, "TRNG buffer: {:?}", buf).unwrap();
-    writeln!(stdout, "TRNG buffer entropy: {} bits per byte", entropy).unwrap();
+    find_and_warn_entropy(stdout, &buf, 6.0, true);
     assert_ne!(buf, [0u8; 15]);
-    assert!(entropy >= 7.0);
 
+    // Large buffer entropy test.
+    let mut buf = [0u8; 80 * 1024]; // 80 KiB.
+    trng.fill_buffer(&mut buf);
+    find_and_warn_entropy(stdout, &buf, 7.5, true);
+
+    // Repeated entropy test.
     for _ in 0..100 {
         let mut buf = [0u8; 103]; // Not a multiple of 32 bits.
         trng.fill_buffer(&mut buf);
-        entropy = min_entropy(&buf);
+        find_and_warn_entropy(stdout, &buf, 6.0, false);
         assert_ne!(buf, [0u8; 103]);
-        assert!(entropy >= 7.0);
 
         let mut buf = [0u8; 124]; // Multiple of 32 bits.
         trng.fill_buffer(&mut buf);
-        entropy = min_entropy(&buf);
+        find_and_warn_entropy(stdout, &buf, 6.0, false);
         assert_ne!(buf, [0u8; 124]);
-        assert!(entropy >= 7.0);
     }
 }
