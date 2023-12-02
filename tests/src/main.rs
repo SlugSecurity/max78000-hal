b58afbf96444425bc8f4f3e1bd130cc969da6c8e
Dmitry Pleshkov <dpleshkov.usa@gmail.com>
2023-12-02 15:53:22 -0800

    functional bit banding tests using Real Time Clock

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -21,12 +21,12 @@ fn main() -> ! {
     let mut stdout = hio::hstdout().unwrap();
     writeln!(stdout, "Starting MAX78000 HAL tests...\n").unwrap();
 
+    // Bit band operations are unsafe due to accessing raw pointers, thus the unsafe block
+    unsafe { bit_band_tests::run_bit_band_tests(&mut stdout) };
+
     // TODO: Use peripheral API when available.
     let peripherals = Peripherals::take().unwrap();
     trng_tests::run_trng_tests(peripherals.TRNG, &peripherals.GCR, &mut stdout);
-
-    // Bit band operations are unsafe, thus the unsafe block
-    unsafe { bit_band_tests::run_bit_band_tests(&mut stdout) };
 
     writeln!(stdout, "Finished MAX78000 HAL tests!\n").unwrap();
 
