1ae7193e5e15e07ae188066f79d32480ab4d693e
Ant <ananthajit5d@gmail.com>
2023-12-02 16:14:23 -0800

    added crc methods, just need tests noaw

--- hal/src/peripherals/crc.rs
+++ hal/src/peripherals/crc.rs
@@ -2,11 +2,27 @@
 
 
 use core::mem;
+use std::io::SeekFrom;
 
 use max78000::CRC;
 
 pub struct Crc {
     crc: CRC,
+}
+
+pub struct CrcReq {
+    data_buffer : [u32],
+    // no need for data len because array length is known at compile timew
+    result_crc :  u32,
+}
+
+enum BitOrder {
+    LSB, 
+    MSB
+}
+
+pub unsafe fn set_field(reg : max78000::generic::Reg<T>, mask : u32, value : u32 ){
+    reg.modify(|r,w| unsafe {w.bits((r.bits() & !mask) | (value & mask) )})
 }
 
 
@@ -17,6 +33,8 @@ impl Crc {
     pub fn new(crc: CRC) -> Self {
         Self {crc}
     }
+
+    
 
 
     // crc_init
@@ -34,14 +52,68 @@ impl Crc {
 
     // shutdown crc
     // might be unsafe since we're changing register values
+    // use a result, is this mut
     pub unsafe fn crc_shutdown(&self) -> () {
-        // self.crc.ctrl.write(|w| unsafe {self.crc.ctrl.read(). }); 
+        self.crc.ctrl.modify(|r, w| unsafe {w.bits(r.bits() & 0x1UL << 0)}); 
         
     }
+
+    pub fn crc_get_result(&self) -> R<REG> {
+        self.crc.val.read()
+    }
+
+    pub fn crc_get_poly(&self) -> R<REG> {
+        self.crc.poly.read()
+    }
+
+
+    pub fn crc_get_direction(&self) -> u32{
+       self.crc.ctrl.read().bits() & (0x1UL << 2)
+        
+    }
+
+    pub unsafe fn crc_set_direction(&self, bitorder : BitOrder){
+
+        set_field(self.crc.ctrl, 0x1UL << 2, (BitOrder as u32) << 2);
+        
+    }
+   
+    pub unsafe fn crc_set_poly(&self, poly : u32){
+        self.crc.poly.write(|w| unsafe {w.bits(poly)});
+    }
+
+    pub unsafe fn crc_swap_in(&self, bitorder : BitOrder){
+        set_field(self.crc.ctrl, 0x1UL << 3, (BitOrder as u32) << 3);
+    }
+
+    pub unsafe fn crc_swap_out(&self, bitorder : BitOrder){
+        set_field(self.crc.ctrl, 0x1UL << 4, (BitOrder as u32) << 4);
+    }
+
+
+    pub unsafe fn crc_compute(&self, crc_req: CrcReq){
+        let mut i = 0;
+        self.crc.ctrl.modify(|r, w| unsafe {r.bits() | 0x1UL } );
+
+        let mut len_counter = crc_req.data_buffer.len();
+
+        while len_counter > 0{
+            self.crc.datain32().write(|w| unsafe {crc_req.data_buffer[i]});
+            i += 1;
+            while self.crc.ctrl.read().bits() & (0x1UL << 16) {};
+
+        }
+
+        crc_req.result_crc = self.crc_get_result();
+    }
+
+    
+// todo: async, confirm with brian
 
 
 
 
 
 }
+
 
