61a723fa85d25993d3a9dc05593fd143a686426c
Dmitry Pleshkov <dpleshkov.usa@gmail.com>
2023-12-02 17:42:27 -0800

    don't steal RTC register block, pass it into test safely. also additional documentation and containment of unsafe blocks

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -21,11 +21,11 @@ fn main() -> ! {
     let mut stdout = hio::hstdout().unwrap();
     writeln!(stdout, "Starting MAX78000 HAL tests...\n").unwrap();
 
-    // Bit band operations are unsafe due to accessing raw pointers, thus the unsafe block
-    unsafe { bit_band_tests::run_bit_band_tests(&mut stdout) };
-
     // TODO: Use peripheral API when available.
     let peripherals = Peripherals::take().unwrap();
+
+    bit_band_tests::run_bit_band_tests(&mut stdout, &peripherals.RTC);
+
     trng_tests::run_trng_tests(peripherals.TRNG, &peripherals.GCR, &mut stdout);
 
     writeln!(stdout, "Finished MAX78000 HAL tests!\n").unwrap();
