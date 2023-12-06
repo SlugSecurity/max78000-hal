27e8c6944667890e2fc3876d97d25f2b4bea0bd2
Dmitry Pleshkov <dpleshkov.usa@gmail.com>
2023-12-05 19:04:59 -0800

    Port over bit banding from tm4c-hal (#20)
    * ported bit banding from tm4c_hal
    
    * make fmt succeed
    
    * tests for change, read, and toggle
    
    * fix the pipeline warnings
    
    * use raw pointer in tests
    
    * sanity check
    
    * further sanity check
    
    * toggle_bit tests needs timings, just test read and set for now
    
    * just make toggle and spin tests placeholders for now
    
    * implemented tests for spin_bit
    
    * hotfix - enable clock and then timer
    
    * hotfix to spinning on wrong register
    
    * spin_bit no longer makes multiple calls to ref_to_bitband
    
    * functional bit banding tests using Real Time Clock
    
    * disable rtc wr_en after test
    
    * don't steal RTC register block, pass it into test safely. also additional documentation and containment of unsafe blocks
    
    * removed unnecessary TODO
    
    * remove arm no-op instruction in loop
    
    * added safety comments to tests and bb methods
    
    * Peg max78000 dependency to a tag (#29)
    
    * Bump max78000 version to v0.2.0 (#30)
    
    * Bump max78000 version and fix changed API
    
    * Update test max78000 version
    
    * Update hal/src/peripherals/bit_banding.rs
    
    Co-authored-by: Brian Mak <eggrollbot@gmail.com>
    
    * Update tests/src/tests/bit_band_tests.rs
    
    Co-authored-by: Brian Mak <eggrollbot@gmail.com>
    
    * Update hal/src/peripherals/bit_banding.rs
    
    Co-authored-by: Brian Mak <eggrollbot@gmail.com>
    
    * Update tests/src/tests/bit_band_tests.rs
    
    Co-authored-by: Brian Mak <eggrollbot@gmail.com>
    
    * Update tests/src/tests/bit_band_tests.rs
    
    Co-authored-by: Brian Mak <eggrollbot@gmail.com>
    
    * ported bit banding from tm4c_hal
    
    * make fmt succeed
    
    * tests for change, read, and toggle
    
    * fix the pipeline warnings
    
    * use raw pointer in tests
    
    * sanity check
    
    * further sanity check
    
    * toggle_bit tests needs timings, just test read and set for now
    
    * just make toggle and spin tests placeholders for now
    
    * implemented tests for spin_bit
    
    * hotfix - enable clock and then timer
    
    * hotfix to spinning on wrong register
    
    * spin_bit no longer makes multiple calls to ref_to_bitband
    
    * functional bit banding tests using Real Time Clock
    
    * disable rtc wr_en after test
    
    * don't steal RTC register block, pass it into test safely. also additional documentation and containment of unsafe blocks
    
    * removed unnecessary TODO
    
    * remove arm no-op instruction in loop
    
    * added safety comments to tests and bb methods
    
    * Update hal/src/peripherals/bit_banding.rs
    
    Co-authored-by: Brian Mak <eggrollbot@gmail.com>
    
    * Update tests/src/tests/bit_band_tests.rs
    
    Co-authored-by: Brian Mak <eggrollbot@gmail.com>
    
    * Update hal/src/peripherals/bit_banding.rs
    
    Co-authored-by: Brian Mak <eggrollbot@gmail.com>
    
    * Update tests/src/tests/bit_band_tests.rs
    
    Co-authored-by: Brian Mak <eggrollbot@gmail.com>
    
    * Update tests/src/tests/bit_band_tests.rs
    
    Co-authored-by: Brian Mak <eggrollbot@gmail.com>
    
    * Updated safety comments to meet convention
    
    ---------
    
    Co-authored-by: Steven <14815068+MelonShooter@users.noreply.github.com>
    Co-authored-by: Brian Mak <eggrollbot@gmail.com>

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -9,7 +9,7 @@ use core::fmt::Write;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hio;
 use max78000_hal::max78000::Peripherals;
-use tests::{oscillator_tests, trng_tests};
+use tests::{bit_band_tests, oscillator_tests, trng_tests};
 
 extern crate panic_halt;
 
@@ -23,6 +23,9 @@ fn main() -> ! {
 
     // TODO: Use peripheral API when available.
     let peripherals = Peripherals::take().unwrap();
+
+    bit_band_tests::run_bit_band_tests(&mut stdout, &peripherals.RTC);
+
     trng_tests::run_trng_tests(peripherals.TRNG, &peripherals.GCR, &mut stdout);
     oscillator_tests::run_oscillator_tests(
         &peripherals.GCR.clkctrl,
