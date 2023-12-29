73418bd6bf189a19856fe86a91a1e6996f6993d8
Astra Tsai <astra@ucsc.edu>
2023-12-28 21:13:49 -0800

    Add some tests on the common GPIO module

--- tests/src/tests/gpio_tests.rs
+++ tests/src/tests/gpio_tests.rs
@@ -4,7 +4,13 @@ use core::fmt::Write;
 use cortex_m_semihosting::hio;
 use max78000_hal::{
     max78000::{GCR, GPIO0, GPIO1, GPIO2, LPGCR, MCR},
-    peripherals::gpio::{new_gpio0, new_gpio1, new_gpio2, new_gpio3},
+    peripherals::gpio::{
+        common::{port_num_types::GpioPortNum, CommonGpio},
+        low_power::LowPowerGpio,
+        new_gpio0, new_gpio1, new_gpio2, new_gpio3,
+        pin_traits::{GeneralIoPin, InputPin, IoPin, OutputPin, PinState, StatefulOutputPin},
+        GpioError, GpioPort, PinIoMode,
+    },
 };
 
 /// Runs all GPIO tests.
@@ -32,7 +38,44 @@ pub fn run_gpio_tests(
 
     // Note: Tests should be made generic over traits like GeneralIoPin, InputPin, and StatefulOutputPin
     // Write sanity checks for now (writing a value then reading it) -- physical tests will come later
-    // TESTS GO BELOW HERE:
+
+    test_common_port(gpio0_port);
+    test_common_port(gpio1_port);
+    test_common_port(gpio2_port);
 
     writeln!(stdout, "GPIO peripheral tests complete!\n").unwrap();
+}
+
+fn test_common_port<const PIN_CT: usize>(
+    port: GpioPort<'static, CommonGpio<impl GpioPortNum + 'static>, PIN_CT>,
+) {
+    let pin = port.get_pin_handle(PIN_CT - 1).unwrap();
+    assert!(matches!(
+        port.get_pin_handle(PIN_CT - 1),
+        Err(GpioError::HandleAlreadyTaken)
+    ));
+    assert!(matches!(
+        port.get_pin_handle(PIN_CT),
+        Err(GpioError::InvalidPinIndex)
+    ));
+
+    let pin = pin.into_input_pin().unwrap();
+    assert!(matches!(pin.get_io_mode(), PinIoMode::Input));
+    assert_ne!(pin.is_low(), pin.is_high());
+
+    let mut pin = pin.into_output_pin(PinState::High).unwrap();
+    assert!(matches!(pin.get_io_mode(), PinIoMode::Output));
+    assert!(pin.is_set_high().unwrap());
+    pin.set_low().unwrap();
+    assert!(pin.is_set_low().unwrap());
+    pin.set_high().unwrap();
+    assert!(pin.is_set_high().unwrap());
+    drop(pin);
+
+    let pin = port.get_pin_handle(PIN_CT - 1).unwrap();
+    assert!(matches!(
+        port.get_pin_handle(PIN_CT - 1),
+        Err(GpioError::HandleAlreadyTaken)
+    ));
+    drop(pin);
 }
