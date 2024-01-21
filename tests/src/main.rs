226934517519679ed1ece4e418f7b71ad1fd5c0a
pdobbins23 <146784496+pdobbins23@users.noreply.github.com>
2024-01-21 13:15:02 -0800

    Power Control API (#27)
    * Added initial power control
    
    * Use en/dis instead of manual bit setting
    
    * Fix TRNG tests with new power API
    
    * not currently working, partial refactor
    
    * bleh bleh bleh bleh bleh bleh
    
    * Fix errors in peripherals::power
    
    * Convert comments above functions into doc comments
    
    * Document Module enum
    
    * Remove unused imports in power.rs
    
    * Fix tests for new power API (again)
    
    * Give GCR and LPGCR separate lifetimes
    
    * Fix incorrect bit selections
    
    * Rename Module -> ToggleableModule
    
    * Add (currently blank) NonToggleableModule enum
    
    * Add reset_non_toggleable instead of many individual reset functions
    
    * Add missing powercontrol modules
    
    * Remove semaphore from NonToggleableModule
    
    * Update todo for CPU1 with correct issue
    
    ---------
    
    Co-authored-by: Natalie Marks <ndmarks@ucsc.edu>
    Co-authored-by: Ben Grant <benjamin.j.grant@gmail.com>

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -8,7 +8,7 @@ use core::fmt::Write;
 
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hio;
-use max78000_hal::max78000::Peripherals;
+use max78000_hal::{max78000::Peripherals, peripherals::power::PowerControl};
 use tests::{bit_band_tests, oscillator_tests, trng_tests};
 
 extern crate panic_halt;
@@ -32,7 +32,10 @@ fn main() -> ! {
         &mut stdout,
     );
 
-    trng_tests::run_trng_tests(peripherals.TRNG, &peripherals.GCR, &mut stdout);
+    // TODO: Assuming peripheral API will initialize this later.
+    let power = PowerControl::new(&peripherals.GCR, &peripherals.LPGCR);
+
+    trng_tests::run_trng_tests(peripherals.TRNG, &power, &mut stdout);
 
     writeln!(stdout, "Finished MAX78000 HAL tests!\n").unwrap();
 
