32b3633a9e072958abaa5b7ddc144f8b46e5178b
Dmitry Pleshkov <dpleshkov.usa@gmail.com>
2024-02-16 18:13:48 -0800

    todo: config

--- hal/src/peripherals/i2c.rs
+++ hal/src/peripherals/i2c.rs
@@ -1,9 +1,10 @@
+use crate::peripherals::i2c::SlavePollResult::{Received, TransmitNeeded};
 use core::ops::Deref;
 use embedded_hal;
 use embedded_hal::i2c::{ErrorKind, ErrorType, Operation, SevenBitAddress};
 use max78000::{i2c0, GCR};
 use max78000::{I2C0, I2C1, I2C2};
-use crate::peripherals::i2c::SlavePollResult::{Received, TransmitNeeded};
+// use cortex_m::interrupt::free;
 
 pub trait GCRI2C {
     /// Disable peripheral
@@ -12,6 +13,16 @@ pub trait GCRI2C {
     fn peripheral_clock_enable(gcr_reg: &GCR);
     /// Reset the peripheral
     fn reset_peripheral(gcr_reg: &GCR);
+    /// Flush transmit and receive FIFO
+    fn flush_fifo(&mut self);
+    /// Is receive FIFO full?
+    fn is_rx_fifo_full(&self) -> bool;
+    /// Is receive FIFO empty?
+    fn is_rx_fifo_empty(&self) -> bool;
+    /// Is transmit FIFO full?
+    fn is_tx_fifo_full(&self) -> bool;
+    /// Is transmit FIFO empty?
+    fn is_tx_fifo_empty(&self) -> bool;
 }
 
 macro_rules! gen_impl_gcri2c {
@@ -32,6 +43,23 @@ macro_rules! gen_impl_gcri2c {
                     .$rstReg()
                     .modify(|_, w| w.$lowercaseName().bit(true));
                 while gcr_reg.$rstReg().read().$lowercaseName().bit() {}
+            }
+            fn flush_fifo(&mut self) {
+                self.rxctrl0().modify(|_, w| w.flush().bit(true));
+                self.txctrl0().modify(|_, w| w.flush().bit(true));
+                while self.rxctrl0().read().flush().bit() || self.txctrl0().read().flush().bit() {}
+            }
+            fn is_rx_fifo_empty(&self) -> bool {
+                self.status().read().rx_em().bit()
+            }
+            fn is_rx_fifo_full(&self) -> bool {
+                self.status().read().rx_full().bit()
+            }
+            fn is_tx_fifo_empty(&self) -> bool {
+                self.status().read().tx_em().bit()
+            }
+            fn is_tx_fifo_full(&self) -> bool {
+                self.status().read().tx_full().bit()
             }
         }
     };
@@ -57,7 +85,7 @@ pub enum I2CMode {
 /// TransmitNeeded indicates you need to call slave_send with the data needed
 pub enum SlavePollResult {
     Received(u32, bool),
-    TransmitNeeded
+    TransmitNeeded,
 }
 
 struct I2CMaster<T: Deref<Target = i2c0::RegisterBlock> + GCRI2C> {
@@ -66,7 +94,7 @@ struct I2CMaster<T: Deref<Target = i2c0::RegisterBlock> + GCRI2C> {
 
 struct I2CSlave<T: Deref<Target = i2c0::RegisterBlock> + GCRI2C> {
     i2c_regs: T,
-    address: SevenBitAddress
+    address: SevenBitAddress,
 }
 
 impl<T: Deref<Target = i2c0::RegisterBlock> + GCRI2C> I2CSlave<T> {
@@ -74,12 +102,14 @@ impl<T: Deref<Target = i2c0::RegisterBlock> + GCRI2C> I2CSlave<T> {
         T::peripheral_clock_enable(gcr_regs);
         T::reset_peripheral(gcr_regs);
 
-        Self {i2c_regs, address}
+        Self { i2c_regs, address }
     }
 
     pub fn slave_poll(&mut self, read_buffer: &mut [u8]) -> Result<SlavePollResult, ErrorKind> {
+        self.i2c_regs.flush_fifo();
         // Wait for I2Cn_INTFL0.addr_match = 1
-        while !self.i2c_regs.intfl0().read().addr_match().bit() {};
+
+        while !self.i2c_regs.intfl0().read().addr_match().bit() {}
 
         if self.i2c_regs.ctrl().read().read().bit() {
             let res = self.slave_recv(read_buffer)?;
@@ -90,12 +120,66 @@ impl<T: Deref<Target = i2c0::RegisterBlock> + GCRI2C> I2CSlave<T> {
     }
 
     fn slave_recv(&mut self, read_buffer: &mut [u8]) -> Result<(u32, bool), ErrorKind> {
+        self.i2c_regs
+            .intfl0()
+            .modify(|_, w| w.addr_match().bit(false));
 
+        let mut num_read = 0;
+        let capacity = read_buffer.len();
+
+        // read to fill read buffer
+        while num_read < capacity && !self.i2c_regs.intfl0().read().done().bit() {
+            while !self.i2c_regs.is_rx_fifo_empty() {
+                if num_read < capacity {
+                    read_buffer[num_read] = self.i2c_regs.fifo().read().data().bits();
+                    num_read += 1;
+                }
+            }
+        }
+
+        let was_it_truncated = !self.i2c_regs.intfl0().read().done().bit();
+
+        // discard remaining bytes that we can't put in the read buffer
+        while !self.i2c_regs.intfl0().read().done().bit() {
+            while !self.i2c_regs.is_rx_fifo_empty() {
+                self.i2c_regs.fifo().read().data().bits();
+                num_read += 1;
+            }
+        }
+
+        Ok((num_read as u32, was_it_truncated))
     }
 
-    pub fn slave_send(&mut self) {
-        todo!();
+    pub fn slave_send(&mut self, buffer: &[u8]) -> u32 {
+        // With I2Cn_CTRL.en = 0, initialize all relevant registers, including specifically for this mode I2Cn_CTRL. clkstr_dis = 0,
+        // I2Cn_TXCTRL0[5:2] = 0x8 and I2Cn_TXCTRL0.preload_mode = 0. Don't forget to program I2Cn_CLKHI.hi and
+        // I2Cn_HSCLK.hsclk_hi with appropriate values satisfying tSU;DAT (and HS tSU;DAT).
+
+        self.i2c_regs.intfl0().modify(|_, w| w.tx_lockout().variant(false));
+        let mut num_written = 0;
+        while num_written < buffer.len() && !self.i2c_regs.intfl0().read().done().bit() {
+            while !self.i2c_regs.is_tx_fifo_full() {
+                if num_written < buffer.len() {
+                    self.i2c_regs.fifo().write(|w| w.data().variant(buffer[num_written]));
+                    num_written += 1;
+                }
+            }
+        }
+
+        // write zeros if we've exceeded buffer but master still wants more
+        while !self.i2c_regs.intfl0().read().done().bit() {
+            while !self.i2c_regs.is_tx_fifo_full() {
+                self.i2c_regs.fifo().write(|w| w.data().variant(0));
+            }
+        }
+
+        // clean up!
+        self.i2c_regs.intfl0().modify(|_, w| w.done().bit(false));
+        self.i2c_regs.inten0().modify(|_, w| w.tx_thd().bit(false));
+
+        num_written as u32
     }
+
 }
 
 // TODO: write code to initialize relevant registers for both master and slave operation
@@ -120,10 +204,12 @@ impl<T: Deref<Target = i2c0::RegisterBlock> + GCRI2C> I2CMaster<T> {
             || self.i2c_regs.txctrl0().read().flush().bit()
         {}
 
-        let bytes_to_read = if read.len() >= 256 {256} else {read.len()};
+        let bytes_to_read = if read.len() >= 256 { 256 } else { read.len() };
 
         // Write the number of data bytes to receive to the I2C receive count field (I2Cn_RXCTRL1.cnt).
-        self.i2c_regs.rxctrl1().modify(|_, w| w.cnt().variant(bytes_to_read as u8));
+        self.i2c_regs
+            .rxctrl1()
+            .modify(|_, w| w.cnt().variant(bytes_to_read as u8));
         // Write the I2C slave address byte to the I2Cn_FIFO register with the R/W bit set to 1
         self.i2c_regs
             .fifo()
@@ -144,7 +230,6 @@ impl<T: Deref<Target = i2c0::RegisterBlock> + GCRI2C> I2CMaster<T> {
     }
 
     fn master_send(&mut self, address: SevenBitAddress, write: &[u8]) -> Result<(), ErrorKind> {
-
         // Let's flush the FIFO buffers
         self.i2c_regs.rxctrl0().modify(|_, w| w.flush().bit(true));
         self.i2c_regs.txctrl0().modify(|_, w| w.flush().bit(true));
@@ -155,19 +240,25 @@ impl<T: Deref<Target = i2c0::RegisterBlock> + GCRI2C> I2CMaster<T> {
         {}
 
         // Write the I2C slave address byte to the I2Cn_FIFO register with the R/W bit set to 0
-        self.i2c_regs.fifo().write(|w| w.data().variant(address << 1));
+        self.i2c_regs
+            .fifo()
+            .write(|w| w.data().variant(address << 1));
 
         // Write the desired data bytes to the I2Cn_FIFO register, up to the size of the transmit FIFO. (e.g., If the transmit
         // FIFO size is 8 bytes, the software may write one address byte and seven data bytes before starting the transaction.)
         let mut num_written = 0;
         for i in 0..write.len() {
-            if self.i2c_regs.status().read().tx_full().bit() {break;}
+            if self.i2c_regs.status().read().tx_full().bit() {
+                break;
+            }
             self.i2c_regs.fifo().write(|w| w.data().variant(write[i]));
             num_written += 1;
         }
 
         // Send a START condition by setting I2Cn_MSTCTRL.start = 1
-        self.i2c_regs.mstctrl().modify(|_, w| w.start().variant(true));
+        self.i2c_regs
+            .mstctrl()
+            .modify(|_, w| w.start().variant(true));
 
         // The controller transmits the slave address byte written to the I2Cn_FIFO register
 
@@ -176,30 +267,33 @@ impl<T: Deref<Target = i2c0::RegisterBlock> + GCRI2C> I2CMaster<T> {
         // TODO: add operation timeouts using timer module
 
         // poll addr_ack
-        while !self.i2c_regs.intfl0().read().addr_ack().bit() {};
+        while !self.i2c_regs.intfl0().read().addr_ack().bit() {}
 
         while num_written < write.len() {
             while !self.i2c_regs.status().read().tx_full().bit() {
-                if num_written >= write.len() {break;}
-                self.i2c_regs.fifo().write(|w| w.data().variant(write[num_written]));
+                if num_written >= write.len() {
+                    break;
+                }
+                self.i2c_regs
+                    .fifo()
+                    .write(|w| w.data().variant(write[num_written]));
                 num_written += 1;
-            };
+            }
         }
 
         // Once the software writes all the desired bytes to the I2Cn_FIFO register; the software should set either
         // I2Cn_MSTCTRL.restart or I2Cn_MSTCTRL.stop.
 
-        self.i2c_regs.mstctrl().modify(|_, w| {w.stop().bit(true)});
+        self.i2c_regs.mstctrl().modify(|_, w| w.stop().bit(true));
 
         // Once the controller sends all the remaining bytes and empties the transmit FIFO, the hardware sets
         // I2Cn_INTFL0.done and proceeds to send out either a RESTART condition if I2Cn_MSTCTRL.restart was set, or a
         // STOP condition if I2Cn_MSTCTRL.stop was set.
 
-        while !self.i2c_regs.intfl0().read().done().bit() {};
+        while !self.i2c_regs.intfl0().read().done().bit() {}
 
         Ok(())
     }
-
 }
 
 impl<T: Deref<Target = i2c0::RegisterBlock> + GCRI2C> ErrorType for I2CMaster<T> {
@@ -209,8 +303,8 @@ impl<T: Deref<Target = i2c0::RegisterBlock> + GCRI2C> ErrorType for I2CMaster<T>
 impl<T: Deref<Target = i2c0::RegisterBlock> + GCRI2C> embedded_hal::i2c::I2c for I2CMaster<T> {
     fn read(&mut self, address: SevenBitAddress, read: &mut [u8]) -> Result<(), Self::Error> {
         let bytes_to_read = read.len();
-        for i in 0..bytes_to_read/256 {
-            self.master_recv(address, &mut read[i*256..])?;
+        for i in 0..bytes_to_read / 256 {
+            self.master_recv(address, &mut read[i * 256..])?;
         }
         let leftover = read.len() - (read.len() % 256);
         self.master_recv(address, &mut read[leftover..])?;
@@ -221,20 +315,25 @@ impl<T: Deref<Target = i2c0::RegisterBlock> + GCRI2C> embedded_hal::i2c::I2c for
         self.master_send(address, write)
     }
 
-    fn write_read(
-        &mut self,
-        _address: SevenBitAddress,
-        _write: &[u8],
-        _read: &mut [u8],
-    ) -> Result<(), Self::Error> {
-        todo!()
+    fn write_read(&mut self, address: SevenBitAddress, write: &[u8], read: &mut [u8]) -> Result<(), Self::Error> {
+        //free(|_| -> Result<(), Self::Error> {
+            self.write(address, write)?;
+            self.read(address, read)?;
+            Ok(())
+        //})
     }
 
-    fn transaction(
-        &mut self,
-        _address: SevenBitAddress,
-        _operations: &mut [Operation<'_>],
-    ) -> Result<(), Self::Error> {
-        todo!()
+    fn transaction(&mut self, address: SevenBitAddress, operations: &mut [Operation<'_>]) -> Result<(), Self::Error> {
+        for operation in operations.iter_mut() {
+            match operation {
+                Operation::Read(read) => {
+                    self.read(address, read)?;
+                }
+                Operation::Write(write) => {
+                    self.write(address, write)?;
+                }
+            }
+        }
+        Ok(())
     }
 }
