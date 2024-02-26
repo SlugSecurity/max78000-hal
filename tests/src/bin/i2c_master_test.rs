c114ca76d7f5bcdcada7daa2f641015b8e41b7b4
Dmitry Pleshkov <dpleshkov.usa@gmail.com>
2024-02-25 23:58:48 -0800

    erm troubleshooting

--- tests/src/bin/i2c_master_test.rs
+++ tests/src/bin/i2c_master_test.rs
@@ -11,6 +11,9 @@ use cortex_m_semihosting::hio;
 use max78000_hal::{max78000::Peripherals, peripherals::i2c, peripherals::timer};
 use max78000_hal::peripherals::timer::{Clock, Oscillator, Prescaler, Time};
 use embedded_hal::i2c::I2c;
+use max78000_hal::peripherals::gpio;
+use max78000_hal::peripherals::gpio::pin_traits::GeneralIoPin;
+use max78000_hal::peripherals::gpio::PinOperatingMode;
 
 extern crate panic_semihosting;
 
@@ -23,6 +26,31 @@ fn main() -> ! {
     writeln!(stdout, "Starting i2c master tests...\n").unwrap();
 
     let peripherals = Peripherals::take().unwrap();
+
+    let gpio0 = gpio::new_gpio0(peripherals.GPIO0);
+
+    /*peripherals.GPIO0.en0().modify(|r, w| w.gpio_en().variant(
+        r.gpio_en().bits() | (((1 << 16) | (1 << 17)))
+    ));
+
+    peripherals.GPIO0.en1().modify(|r, w| w.gpio_en1().variant(
+        r.gpio_en1().bits() & (!((1 << 16) | (1 << 17)))
+    ));
+
+    peripherals.GPIO0.en2().modify(|r, w| w.gpio_en2().variant(
+        r.gpio_en2().bits() & (!((1 << 16) | (1 << 17)))
+    ));
+
+    peripherals.GPIO0.en0().modify(|r, w| w.gpio_en().variant(
+        r.gpio_en().bits() & (!((1 << 16) | (1 << 17)))
+    ));*/
+
+    let mut scl_handle = gpio0.get_pin_handle(16).unwrap();
+    let mut sda_handle = gpio0.get_pin_handle(17).unwrap();
+
+    sda_handle.set_operating_mode(PinOperatingMode::AltFunction1);
+    scl_handle.set_operating_mode(PinOperatingMode::AltFunction1);
+
     let mut i2c_master = i2c::I2CMaster::new(&peripherals.GCR, peripherals.I2C1);
     let clock = Clock::new(peripherals.TMR, &peripherals.GCR, Oscillator::IBRO, Prescaler::_64);
 
@@ -36,7 +64,7 @@ fn main() -> ! {
 
     let mut stuff = [0u8; 16];
 
-    i2c_master.read(69, &mut stuff).unwrap();
+    i2c_master.write(0, "ping".as_bytes()).unwrap();
 
     writeln!(stdout, "Finished i2c master tests!\n").unwrap();
 
--- tests/src/bin/i2c_slave_test.rs
+++ tests/src/bin/i2c_slave_test.rs
@@ -12,6 +12,9 @@ use max78000_hal::{max78000::Peripherals, peripherals::i2c, peripherals::timer};
 use max78000_hal::peripherals::timer::{Clock, Oscillator, Prescaler, Time};
 use embedded_hal::i2c::I2c;
 use max78000_hal::peripherals::i2c::SlavePollResult;
+use max78000_hal::peripherals::gpio;
+use max78000_hal::peripherals::gpio::pin_traits::GeneralIoPin;
+use max78000_hal::peripherals::gpio::PinOperatingMode;
 
 extern crate panic_semihosting;
 
@@ -24,6 +27,31 @@ fn main() -> ! {
     writeln!(stdout, "Starting i2c slave tests...\n").unwrap();
 
     let peripherals = Peripherals::take().unwrap();
+
+    let gpio0 = gpio::new_gpio0(peripherals.GPIO0);
+
+    /*peripherals.GPIO0.en0().modify(|r, w| w.gpio_en().variant(
+        r.gpio_en().bits() | (((1 << 16) | (1 << 17)))
+    ));
+
+    peripherals.GPIO0.en1().modify(|r, w| w.gpio_en1().variant(
+        r.gpio_en1().bits() & (!((1 << 16) | (1 << 17)))
+    ));
+
+    peripherals.GPIO0.en2().modify(|r, w| w.gpio_en2().variant(
+        r.gpio_en2().bits() & (!((1 << 16) | (1 << 17)))
+    ));
+
+    peripherals.GPIO0.en0().modify(|r, w| w.gpio_en().variant(
+        r.gpio_en().bits() & (!((1 << 16) | (1 << 17)))
+    ));*/
+
+    let mut scl_handle = gpio0.get_pin_handle(16).unwrap();
+    let mut sda_handle = gpio0.get_pin_handle(17).unwrap();
+
+    sda_handle.set_operating_mode(PinOperatingMode::AltFunction1);
+    scl_handle.set_operating_mode(PinOperatingMode::AltFunction1);
+
     let mut i2c_slave = i2c::I2CSlave::new(&peripherals.GCR, peripherals.I2C1, 69);
     let clock = Clock::new(peripherals.TMR, &peripherals.GCR, Oscillator::IBRO, Prescaler::_64);
 
@@ -36,6 +64,18 @@ fn main() -> ! {
     writeln!(stdout, "Ok we poll now\n").unwrap();
 
     let mut buf = [0u8; 256];
+
+    /*let mut scl = peripherals.GPIO0.in_().read().bits() & (1 << 16);
+    let mut sda = peripherals.GPIO0.in_().read().bits() & (1 << 17);
+
+    let read_sda = || peripherals.GPIO0.in_().read().bits() & (1 << 17);
+    let read_scl = || peripherals.GPIO0.in_().read().bits() & (1 << 16);
+
+    loop {
+        while read_scl() == scl {};
+        scl = read_scl();
+        writeln!(stdout, "SDA changed!!\n").unwrap();
+    }*/
 
     if let SlavePollResult::Received(num, overflow) = i2c_slave.slave_poll(&mut buf).unwrap() {
         writeln!(stdout, stringify!(buf)).unwrap();
