829cf4709862d2c673a25d7d3782fe0e4c52b46c
Ben Grant <benjamin.j.grant@gmail.com>
2024-02-27 16:40:47 -0800

    More UART testing

--- tests/src/tests/uart_tests.rs
+++ tests/src/tests/uart_tests.rs
@@ -11,6 +11,47 @@ use max78000_hal::peripherals::{
     PeripheralHandle,
 };
 
+const INTERJECTION: [u8; 2568] =
+    *b"I'd just like to interject for a moment.  What you're referring to as Linux,
+is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux.
+Linux is not an operating system unto itself, but rather another free component
+of a fully functioning GNU system made useful by the GNU corelibs, shell
+utilities and vital system components comprising a full OS as defined by POSIX.
+
+Many computer users run a modified version of the GNU system every day,
+without realizing it.  Through a peculiar turn of events, the version of GNU
+which is widely used today is often called \"Linux\", and many of its users are
+not aware that it is basically the GNU system, developed by the GNU Project.
+
+There really is a Linux, and these people are using it, but it is just a
+part of the system they use.  Linux is the kernel: the program in the system
+that allocates the machine's resources to the other programs that you run.
+The kernel is an essential part of an operating system, but useless by itself;
+it can only function in the context of a complete operating system.  Linux is
+normally used in combination with the GNU operating system: the whole system
+is basically GNU with Linux added, or GNU/Linux.  All the so-called \"Linux\"
+distributions are really distributions of GNU/Linux.
+I'd just like to interject for a moment.  What you're referring to as Linux,
+is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux.
+Linux is not an operating system unto itself, but rather another free component
+of a fully functioning GNU system made useful by the GNU corelibs, shell
+utilities and vital system components comprising a full OS as defined by POSIX.
+
+Many computer users run a modified version of the GNU system every day,
+without realizing it.  Through a peculiar turn of events, the version of GNU
+which is widely used today is often called \"Linux\", and many of its users are
+not aware that it is basically the GNU system, developed by the GNU Project.
+
+There really is a Linux, and these people are using it, but it is just a
+part of the system they use.  Linux is the kernel: the program in the system
+that allocates the machine's resources to the other programs that you run.
+The kernel is an essential part of an operating system, but useless by itself;
+it can only function in the context of a complete operating system.  Linux is
+normally used in combination with the GNU operating system: the whole system
+is basically GNU with Linux added, or GNU/Linux.  All the so-called \"Linux\"
+distributions are really distributions of GNU/Linux.
+";
+
 /// Run all UART tests
 pub fn run_uart_test(
     stdout: &mut hio::HostStream,
@@ -49,9 +90,6 @@ pub fn run_uart_test(
     timer = clk0.new_timer(Milliseconds(500));
     while uart.recv_with_timeout(&mut byte, &mut timer) != Ok(1) || byte != [0xff] {}
 
-    // ping the host to say we are ready for the next test
-    // uart.send(&mut buf[0..1]).unwrap();
-
     timer = clk0.new_timer(Milliseconds(500));
     buf = [0; 14];
     assert_eq!(uart.recv_with_data_timeout(&mut buf, &mut timer), Ok(14));
@@ -60,5 +98,23 @@ pub fn run_uart_test(
         *b"womp womp womp",
         "actually received: {}",
         core::str::from_utf8(&buf).unwrap_or("[junk]")
+    );
+
+    // long receive test
+    let mut big_buf = [0u8; 2568];
+    timer = clk0.new_timer(Milliseconds(5000));
+    let ret = uart.recv_with_data_timeout(&mut big_buf, &mut timer);
+    for (i, &byte) in big_buf.iter().enumerate() {
+        if byte != INTERJECTION[i] {
+            writeln!(stdout, "first mismatch at {i}").unwrap();
+            break;
+        }
+    }
+    assert_eq!(ret, Ok(big_buf.len()));
+    assert_eq!(
+        big_buf,
+        INTERJECTION,
+        "actually received: {}",
+        core::str::from_utf8(&big_buf).unwrap_or("[junk]")
     );
 }
--- tests/uart_tester.py
+++ tests/uart_tester.py
@@ -5,6 +5,29 @@ import os
 import unittest
 import time
 
+interjection = str.encode("""I'd just like to interject for a moment.  What you're referring to as Linux,
+is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux.
+Linux is not an operating system unto itself, but rather another free component
+of a fully functioning GNU system made useful by the GNU corelibs, shell
+utilities and vital system components comprising a full OS as defined by POSIX.
+
+Many computer users run a modified version of the GNU system every day,
+without realizing it.  Through a peculiar turn of events, the version of GNU
+which is widely used today is often called "Linux", and many of its users are
+not aware that it is basically the GNU system, developed by the GNU Project.
+
+There really is a Linux, and these people are using it, but it is just a
+part of the system they use.  Linux is the kernel: the program in the system
+that allocates the machine's resources to the other programs that you run.
+The kernel is an essential part of an operating system, but useless by itself;
+it can only function in the context of a complete operating system.  Linux is
+normally used in combination with the GNU operating system: the whole system
+is basically GNU with Linux added, or GNU/Linux.  All the so-called "Linux"
+distributions are really distributions of GNU/Linux.
+""")
+
+print(len(interjection))
+
 class UartTest(unittest.TestCase):
 	def setUp(self):
 		serial_id = os.environ['SERIAL']
@@ -37,6 +60,18 @@ class UartTest(unittest.TestCase):
 			time.sleep(0.1)
 			self.port.write(to_send[i:i+1])
 			self.port.flush()
+
+	def test_4_long_recv(self):
+		total = 0
+		to_send = interjection * 2
+		while total < len(to_send):
+			current = self.port.write(to_send[total:])
+			print(f'wrote {current} bytes')
+			total += current
+		self.port.flush()
+
+	def test_5_long_transmit(self):
+		pass
 
 	def tearDown(self):
 		self.port.close()
