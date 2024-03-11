23f9da443cfe78fb95bccdb68bc9f31b969e1d8b
GrapeJuiceSoda <kendallcarmel@gmail.com>
2024-03-10 17:44:13 -0700

    fix test string

--- tests/uart_tester.py
+++ tests/uart_tester.py
@@ -93,22 +93,22 @@ class UartTest(unittest.TestCase):
 		self.port.flush()
 
 	def test_5_recv_formatted1(self):
+		self.port.reset_input_buffer()
 		sent_test_string = b'%debug: this is a debug message%'
 		sent = self.port.read(len(sent_test_string))
 		self.assertEqual(sent, sent_test_string)
-		self.port.flush()
 
 	def test_6_recv_formatted3(self):
-		sent_test_string = b'A'*128
+		self.port.reset_input_buffer()
+		sent_test_string = b'This is a uart test, what is uart? uart is a your art. uart is universal art. uart is uncanny art.'
 		sent = self.port.read(len(sent_test_string))
 		self.assertEqual(sent, sent_test_string)
-		self.port.flush()
 
 	def test_7_recv_formatted2(self):
+		self.port.reset_input_buffer()
 		sent_test_string = b'%info: DATE>1/1/1970%'
 		sent = self.port.read(len(sent_test_string))
 		self.assertEqual(sent, sent_test_string)
-		self.port.flush()
 
 	def tearDown(self):
 		self.port.close()
