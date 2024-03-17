f0432bb1ed5ec7c5f8ef018f8dbe4a8c07c4af89
GrapeJuiceSoda <kendallcarmel@gmail.com>
2024-03-16 22:40:31 -0700

    added critical r/w tests

--- tests/src/tests/fip_tests.rs
+++ tests/src/tests/fip_tests.rs
@@ -3,6 +3,7 @@ use core::{borrow::BorrowMut, ptr::write_volatile};
 use cortex_m_semihosting::hio;
 use fault_injection_protection_arm as fip;
 use fip::{FaultInjectionPrevention, SecureBool};
+use max78000_hal::communication::lower_layers::crypto::RandomSource;
 use max78000_hal::peripherals::{rand_chacha::ChaChaRng, PeripheralHandle};
 use subtle::ConstantTimeEq;
 
@@ -36,42 +37,46 @@ fn test_critical_if(
         csprng,
     );
 
-    // writeln!(stdout, "Testing using rng in critical-if closure").unwrap();
-    // let secure_bool = SecureBool::Error;
-    // fip.critical_if(
-    //     || true.into(),
-    //     |rng: &mut ChaChaRng| {
-    //         fip.critical_write(
-    //             &mut secure_bool,
-    //             SecureBool::True,
-    //             |dst, src| unsafe { write_volatile(dst, src) },
-    //             rng,
-    //         )
-    //     },
-    //     || (),
-    //     csprng,
-    // );
+    writeln!(stdout, "Testing using rng in critical-if closure").unwrap();
+    let mut secure_buffer = [0u8; 24];
+    csprng.fill_rand_slice(&mut secure_buffer);
 
-    // fip.critical_if(
-    //     || {
-    //         SecureBool::from(<subtle::Choice as Into<bool>>::into(
-    //             secure_bool.ct_eq(&SecureBool::True),
-    //         ))
-    //     },
-    //     || {
-    //         writeln!(
-    //             stdout,
-    //             "Success: critical-write work inside of critical-if closure"
-    //         )
-    //         .unwrap()
-    //     },
-    //     || writeln!(stdout, "Error: critical-write fails").unwrap(),
-    //     csprng,
-    // );
+    let mut other_secure_buffer = [0u8; 24];
 
-    // if fip.critical_read(&secure_bool, csprng) == SecureBool::True {
-    //     writeln!(stdout, "Success: critical-read works").unwrap();
-    // } else {
-    //     writeln!(stdout, "Error: critical-read fails").unwrap();
-    // }
+    fip.critical_if(
+        || true.into(),
+        |rng: &mut ChaChaRng| {
+            fip.critical_write(
+                &mut other_secure_buffer,
+                secure_buffer,
+                |dst, src| unsafe { write_volatile(dst, src) },
+                rng,
+            )
+        },
+        || (),
+        csprng,
+    );
+
+    fip.critical_if(
+        || {
+            SecureBool::from(<subtle::Choice as Into<bool>>::into(
+                other_secure_buffer.ct_eq(&secure_buffer),
+            ))
+        },
+        || {
+            writeln!(
+                stdout,
+                "Success: critical-write work inside of critical-if closure"
+            )
+            .unwrap()
+        },
+        || panic!("Error: critical-write fails"),
+        csprng,
+    );
+
+    if fip.critical_read(&other_secure_buffer, csprng) == secure_buffer {
+        writeln!(stdout, "Success: critical-read works").unwrap();
+    } else {
+        writeln!(stdout, "Error: critical-read fails").unwrap();
+    }
 }
