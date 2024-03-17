f678c781a35588e676bb312b9e08c191156e67d3
GrapeJuiceSoda <kendallcarmel@gmail.com>
2024-03-16 22:35:02 -0700

    added fip tests

---
+++ tests/src/tests/fip_tests.rs
@@ -0,0 +1,77 @@
+use core::fmt::Write;
+use core::{borrow::BorrowMut, ptr::write_volatile};
+use cortex_m_semihosting::hio;
+use fault_injection_protection_arm as fip;
+use fip::{FaultInjectionPrevention, SecureBool};
+use max78000_hal::peripherals::{rand_chacha::ChaChaRng, PeripheralHandle};
+use subtle::ConstantTimeEq;
+
+pub fn run_fip_tests(mut csprng: PeripheralHandle<'_, ChaChaRng>, stdout: &mut hio::HostStream) {
+    writeln!(stdout, "Starting fip peripheral tests...").unwrap();
+
+    let fip = fip::FaultInjectionPrevention::new();
+    test_critical_if(&fip, csprng.borrow_mut(), stdout);
+
+    // Run tests.
+    writeln!(stdout, "FIP tests complete!\n").unwrap();
+}
+
+/// Tests the [`ChaCha20Rng::fill_bytes()`] function for the initialized CSPRNG.
+fn test_critical_if(
+    fip: &FaultInjectionPrevention,
+    csprng: &mut ChaChaRng,
+    stdout: &mut hio::HostStream,
+) {
+    fip.critical_if(
+        || (true == true).into(),
+        || writeln!(stdout, "Success: True path").unwrap(),
+        || panic!("Error: False path"),
+        csprng,
+    );
+
+    fip.critical_if(
+        || (true == false).into(),
+        || panic!("Error: True path"),
+        || writeln!(stdout, "Success: False path").unwrap(),
+        csprng,
+    );
+
+    // writeln!(stdout, "Testing using rng in critical-if closure").unwrap();
+    // let secure_bool = SecureBool::Error;
+    // fip.critical_if(
+    //     || true.into(),
+    //     |rng: &mut ChaChaRng| {
+    //         fip.critical_write(
+    //             &mut secure_bool,
+    //             SecureBool::True,
+    //             |dst, src| unsafe { write_volatile(dst, src) },
+    //             rng,
+    //         )
+    //     },
+    //     || (),
+    //     csprng,
+    // );
+
+    // fip.critical_if(
+    //     || {
+    //         SecureBool::from(<subtle::Choice as Into<bool>>::into(
+    //             secure_bool.ct_eq(&SecureBool::True),
+    //         ))
+    //     },
+    //     || {
+    //         writeln!(
+    //             stdout,
+    //             "Success: critical-write work inside of critical-if closure"
+    //         )
+    //         .unwrap()
+    //     },
+    //     || writeln!(stdout, "Error: critical-write fails").unwrap(),
+    //     csprng,
+    // );
+
+    // if fip.critical_read(&secure_bool, csprng) == SecureBool::True {
+    //     writeln!(stdout, "Success: critical-read works").unwrap();
+    // } else {
+    //     writeln!(stdout, "Error: critical-read fails").unwrap();
+    // }
+}
